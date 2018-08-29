8a7f0e36c5ff3e9b90c4ccb0efa52b820494db26
Jason White <31048576+drunkenkismet@users.noreply.github.com>
2018-08-29 10:12:19 -0400

    Updated comments

--- js/resources.js
+++ js/resources.js
@@ -1,8 +1,8 @@
 /* Resources.js
  * This is simply an image loading utility. It eases the process of loading
- * image files so that they can be used within your game. It also includes
- * a simple "caching" layer so it will reuse cached images if you attempt
- * to load the same image multiple times.
+ * image files so that they can be used within the game. It also includes
+ * a simple "caching" layer so it will reuse cached images if the same
+ * image is loaded multiple times.
  */
 (function() {
     var resourceCache = {};
@@ -11,22 +11,14 @@
 
     /* This is the publicly accessible image loading function. It accepts
      * an array of strings pointing to image files or a string for a single
-     * image. It will then call our private image loading function accordingly.
+     * image. It will then call the private image loading function accordingly.
      */
     function load(urlOrArr) {
         if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
-             */
             urlOrArr.forEach(function(url) {
                 _load(url);
             });
         } else {
-            /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
-             * directly.
-             */
             _load(urlOrArr);
         }
     }
@@ -37,24 +29,23 @@
     function _load(url) {
         if(resourceCache[url]) {
             /* If this URL has been previously loaded it will exist within
-             * our resourceCache array. Just return that image rather
+             * our resourceCache array. It returns that image rather than
              * re-loading the image.
              */
             return resourceCache[url];
         } else {
             /* This URL has not been previously loaded and is not present
-             * within our cache; we'll need to load this image.
+             * within our cache; load this image.
              */
             var img = new Image();
             img.onload = function() {
-                /* Once our image has properly loaded, add it to our cache
-                 * so that we can simply return this image if the developer
-                 * attempts to load this file in the future.
+                /* Once the image has properly loaded, add it to the cache
+                 * to return this image if the image file is needed in the future.
                  */
                 resourceCache[url] = img;
 
                 /* Once the image is actually loaded and properly cached,
-                 * call all of the onReady() callbacks we have defined.
+                 * call all of the defined onReady() callbacks.
                  */
                 if(isReady()) {
                     readyCallbacks.forEach(function(func) { func(); });
@@ -62,17 +53,16 @@
             };
 
             /* Set the initial cache value to false, this will change when
-             * the image's onload event handler is called. Finally, point
-             * the image's src attribute to the passed in URL.
+             * the image's onload event handler is called. Then point
+             * the image's src attribute to the passed-in URL.
              */
             resourceCache[url] = false;
             img.src = url;
         }
     }
 
-    /* This is used by developers to grab references to images they know
-     * have been previously loaded. If an image is cached, this functions
-     * the same as calling load() on that URL.
+    /* This is used to grab references to previously loaded images. 
+     * If an image is cached, this functions the same as calling load() on that URL.
      */
     function get(url) {
         return resourceCache[url];
@@ -99,8 +89,7 @@
         readyCallbacks.push(func);
     }
 
-    /* This object defines the publicly accessible functions available to
-     * developers by creating a global Resources object.
+    /* This object defines the publicly accessible functions available by creating a global Resources object.
      */
     window.Resources = {
         load: load,
